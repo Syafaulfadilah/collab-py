caf74543a4bcda67378d5b649ca6d3bee9205e72
Syafaulfadilah <116011762+Syafaulfadilah@users.noreply.github.com>
2022-12-09 23:48:21 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# collab-py
 Python Apps Spelling
---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, render_template, url_for
+from symspellpy import SymSpell
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/deteksi')
+def deteksi():
+    return render_template('deteksi.html')
+
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    hasil = request.form['textoutput']
+    input = request.form['textinput']
+
+    
+    #lookup suggestions for multi-word input strings (supports compound splitting & merging)
+    sentence = input.split()
+    #initialize
+    sym_spell = SymSpell()
+    # create dictionary
+    path_corpus = "kompas.txt"
+    sym_spell.create_dictionary(path_corpus)
+
+    for kata in sentence:
+                suggestion = suggestions = sym_spell.lookup_compound(
+    input, max_edit_distance=2, transfer_casing=True,
+)
+
+            #display term and edit distance
+                for suggestion in suggestion:
+                    hasil = suggestion
+    return render_template('deteksi.html', hasil = hasil, input=input )
+if __name__ == '__main__':
+    app.run()
